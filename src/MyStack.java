5518336a93a69ac3cf11e4f5df1d82bbc7dd9675
HannaRaus <raus.a.hanna@gmail.com>
2021-02-16 21:44:02 +0200

    changed MyStack as a child from ConnectedList

--- src/MyStack.java
+++ src/MyStack.java
@@ -1,17 +1,16 @@
-public class MyStack<E> extends MyLinkedList<E> {
+public class MyStack<E> extends ConnectedList<E> {
 
     public void push(E element) {
         super.add(element);
     }
 
     public E peek() {
-        return first.item;
+        return last.item;
     }
 
     public E pop() {
-        E temp = first.item;
+        E temp = last.item;
         remove(0);
         return temp;
     }
-
 }
